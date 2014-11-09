98da2148c21d9c5443c3ed434e6c7327a8b75785
Vanessa Barbosa <vanessabmdf@gmail.com>
2014-11-09 17:57:29 -0200

    Versão 9

--- InvestMVC/grails-app/conf/BootStrap.groovy
+++ InvestMVC/grails-app/conf/BootStrap.groovy
@@ -14,28 +14,4 @@ class BootStrap {
 
     def destroy = {
     }
-/*
-    public void createAdmin(){
-    	def adminUser = new User (name:"admin",lastName:"admin", email:"admin@com", password:"admin",enabled:true,accountExpired:false,accountLocked:false,passwordExpired:false)
-    	adminUser.save(flush:true)
-
-        println adminUser.name
-
-    	def role = new Role (authority: 'ROLE_ADMIN');
-        role.save(flush:true)
-
-        println "Autorizacao criado"
-
-    	UserRole.create(adminUser, role, true)
-    }
-
-    public void createUser(){
-    	def adminUser = new User (username:"user",password:"user",enabled:true,accountExpired:false,accountLocked:false,passwordExpired:false)
-    	adminUser.save(flush:true)
-
-    	def role = new Role (authority: 'User');
-        role.save(flush:true)
-
-    	UserRole.create(adminUser, role, true)
-    }*/
 }
